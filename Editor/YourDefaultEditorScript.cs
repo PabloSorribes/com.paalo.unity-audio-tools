95c99e5e9d4d78b7b96012b77ac2e5f588682d34
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2020-02-08 00:34:20 +0100

    empty package

---
+++ Editor/YourDefaultEditorScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace PabloSorribes.UnityAudioTools
+{
+	public class YourDefaultEditorScript : Editor 
+	{
+		//TestyTest    
+	}
+}
---
+++ Runtime/YourDefaultBehaviourScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PabloSorribes.UnityAudioTools
+{
+	public class YourDefaultBehaviourScript : MonoBehaviour 
+	{
+		//TestyTest    
+	}
+}
