1bf623d5567d62526d193680f31c85ca7aa34004
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2020-02-08 01:34:42 +0100

    added the AudioVolumeConverterTool

---
+++ Editor/Convert between Decibel and Linear Value/AudioVolumeConverterTool.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace PabloSorribes.UnityAudioTools.Editor
+{
+	public class AudioVolumeConverterTool : EditorWindow
+	{
+		private const string menuItemPath = "PabloSorribes/Tools/Audio Volume Converter Tool";
+		private static readonly string myWindowTitle = "Audio Volume Converter Tool";
+
+		[MenuItem(menuItemPath)]
+		public static void SetupWindow()
+		{
+			//Instantiate the window and set its size.
+			var window = GetWindow<AudioVolumeConverterTool>(utility: false, title: myWindowTitle, focus: true);
+			window.minSize = new Vector2(400, 65);
+			window.maxSize = new Vector2(window.minSize.x + 10, window.minSize.y + 10);
+			window.Show();
+		}
+
+		public void OnEnable()
+		{
+			var root = this.rootVisualElement;
+			root.style.paddingTop = new StyleLength(10f);
+			root.style.paddingBottom = new StyleLength(10f);
+			root.style.paddingLeft = new StyleLength(10f);
+			root.style.paddingRight = new StyleLength(10f);
+
+			UnityEngine.UIElements.Toggle performanceToggle = new UnityEngine.UIElements.Toggle("Use approximation for performance: ");
+			performanceToggle.value = false;
+			root.Add(performanceToggle);
+
+			UnityEditor.UIElements.FloatField decibelField = new UnityEditor.UIElements.FloatField("Decibel (dB)");
+			root.Add(decibelField);
+
+			UnityEditor.UIElements.FloatField amplitudeField = new UnityEditor.UIElements.FloatField("Normalized Value (0-1)");
+			root.Add(amplitudeField);
+
+			amplitudeField.value = AudioVolumeConverter.ConvertDecibelVolumeToLinearVolume(decibelField.value, performanceToggle.value);
+
+			decibelField.RegisterCallback<ChangeEvent<float>>(evt =>
+			{
+				amplitudeField.value = AudioVolumeConverter.ConvertDecibelVolumeToLinearVolume(decibelField.value, performanceToggle.value);
+				amplitudeField.value = Mathf.Clamp01(amplitudeField.value);
+				decibelField.value = Mathf.Clamp(decibelField.value, -80f, 0f);
+			});
+
+
+			amplitudeField.RegisterCallback<ChangeEvent<float>>(evt =>
+			{
+				decibelField.value = AudioVolumeConverter.ConvertLinearVolumeToDecibelVolume(amplitudeField.value, performanceToggle.value);
+				decibelField.value = Mathf.Clamp(decibelField.value, -80f, 0f);
+				amplitudeField.value = Mathf.Clamp01(amplitudeField.value);
+			});
+		}
+	}
+}
---
+++ Runtime/Convert between Decibel and Linear Value/AudioVolumeConverter.cs
@@ -0,0 +1,63 @@
+﻿// Source: "FPSSample"-project by Unity, in the SoundSystem-class
+// https://github.com/Unity-Technologies/FPSSample/blob/master/Assets/Scripts/Audio/SoundSystem.cs
+
+using UnityEngine;
+
+namespace PabloSorribes.UnityAudioTools
+{
+	/// <summary>
+	/// Helper class for helping to input Sound Designer-friendly Decibel values, 
+	/// which get converted into Parametric values (0-1f), which are better for Unity's built in Audio Classes.
+	/// Also contains a function for converting from Parametric to Decibel.
+	/// </summary>
+	public static class AudioVolumeConverter
+	{
+		public static readonly float SOUND_DB_CUTOFF = -60.0f;
+		public static readonly float SOUND_LINEAR_CUTOFF = Mathf.Pow(2.0f, SOUND_DB_CUTOFF / 6.02f);
+
+		/// <summary>
+		/// Returns a decibel value based on a parametric value (0-1f).
+		/// </summary>
+		/// <param name="linearVolume"></param>
+		/// <returns></returns>
+		public static float ConvertLinearVolumeToDecibelVolume(float linearVolume, bool usePerformanceApproximation = true)
+		{
+			float dBVolume = 0f;
+
+			if (usePerformanceApproximation)
+			{
+				if (linearVolume < SOUND_LINEAR_CUTOFF)
+					return -60.0f;
+
+				dBVolume = 6.02f * Mathf.Log(linearVolume) / Mathf.Log(2.0f);
+			}
+			else
+			{
+				dBVolume = 20f * Mathf.Log10(linearVolume);
+			}
+			return dBVolume;
+		}
+
+		/// <summary>
+		/// Returns the parametric value (0-1f) corresponding to the inputted decibel value.
+		/// </summary>
+		/// <param name="dBVolume"></param>
+		/// <returns></returns>
+		public static float ConvertDecibelVolumeToLinearVolume(float dBVolume, bool usePerformanceApproximation = true)
+		{
+			float linearVolume = 0f;
+
+			if (usePerformanceApproximation)
+			{
+				if (dBVolume <= SOUND_DB_CUTOFF)
+					return 0;
+				linearVolume = Mathf.Pow(2.0f, dBVolume / 6.02f);
+			}
+			else
+			{
+				linearVolume = Mathf.Pow(10f, dBVolume / 20f);
+			}
+			return linearVolume;
+		}
+	}
+}
