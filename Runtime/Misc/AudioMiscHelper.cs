ef99bfbd20416f79deab53a56ded9f1dd9676ce7
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2020-06-05 03:18:42 +0200

    Adding GetAudioPlayedPercentage from Gumslinger

--- Runtime/Misc/AudioMiscHelper.cs
+++ Runtime/Misc/AudioMiscHelper.cs
@@ -23,5 +23,19 @@ namespace Paalo.UnityAudioTools
 			return clipDuration;
 		}
 
+		/// <summary>
+        /// This method will tell you the percentage of the clip that is done Playing (0-100).
+        /// </summary>
+        /// <param name="source">The Audio Source to calculate for.</param>
+        /// <returns>(0-100 float)</returns>
+        public static float GetAudioPlayedPercentage(AudioSource source) {
+            if (source.clip == null || source.time == 0f) {
+                return 0f;
+            }
+
+            var playedPercentage = (source.time / source.clip.length) * 100;
+            return playedPercentage;
+        }
+
 	}
 }
